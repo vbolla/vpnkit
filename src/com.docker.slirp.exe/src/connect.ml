ba9863b14d186e9f62fd67d2c50c542c1d689184
David Scott <dave@recoil.org>
2016-06-10 11:06:50 -0700

    HACK: connect to 10.0.75.2
    Signed-off-by: David Scott <dave@recoil.org>

--- src/com.docker.slirp.exe/src/connect.ml
+++ src/com.docker.slirp.exe/src/connect.ml
@@ -11,7 +11,7 @@ let set_port_forward_addr x = hvsockaddr := Some x
 
 type port = Hostnet.Forward.Port.t
 
-include Flow_lwt_hvsock_shutdown
+include Hostnet.Conn_lwt_unix
 
 open Lwt.Infix
 
@@ -19,24 +19,13 @@ let connect port = match !hvsockaddr with
   | None ->
     Log.err (fun f -> f "Please set a Hyper-V socket address for port forwarding");
     failwith "Hyper-V socket forwarding not initialised"
-  | Some sockaddr ->
-    let fd = Lwt_hvsock.create () in
-    Lwt_hvsock.connect fd sockaddr
-    >>= fun () ->
-    (* Matches the Go definition *)
-    let proto, ip, port = match port with
-      | `Tcp(ip, port) -> 1, ip, port
-      | `Udp(ip, port) -> 2, ip, port in
-    let header = Cstruct.create (1 + 2 + 4 + 2) in
-    Cstruct.set_uint8 header 0 proto;
-    Cstruct.LE.set_uint16 header 1 4;
-    let ip = Ipaddr.V4.to_bytes ip in
-    Cstruct.blit_from_string ip 0 header 3 4;
-    Cstruct.LE.set_uint16 header 7 port;
-    let flow = connect fd in
-    write flow header
-    >>= function
-    | `Eof -> Lwt.fail End_of_file
-    | `Error e -> Lwt.fail (Failure (error_message e))
-    | `Ok () ->
-      Lwt.return flow
+  | Some _sockaddr ->
+    begin match port with
+    | `Tcp(_ip, _port) ->
+      let sockaddr = Unix.ADDR_INET(Unix.inet_addr_of_string "10.0.75.2", 8080) in
+      let fd = Lwt_unix.socket Lwt_unix.PF_INET Lwt_unix.SOCK_STREAM 0 in
+      Lwt_unix.connect fd sockaddr
+      >>= fun () ->
+      Lwt.return (connect fd)
+    | `Udp(_ip, _port) -> failwith "unimplemented"
+    end 
